85bde4f23fdc3d3119f9c4643367fd364a6b8a33
koln-lv <koln@coolmogo.com>
2026-05-21 08:51:20 +0800

    user update

--- lib/core/tasks/model/domain/task_activity_model.dart
+++ lib/core/tasks/model/domain/task_activity_model.dart
@@ -1,35 +1,47 @@
+import '../../../users/model/user_model.dart';
+
 enum ActivityType { comment, history }
 
 class ActivityLog {
   final String id;
   final String text;
-  final String authorName;
+  final User? author;
+  final String? legacyAuthorName;
   final DateTime timestamp;
   final ActivityType type;
 
   const ActivityLog({
     required this.id,
     required this.text,
-    required this.authorName,
+    this.author,
+    this.legacyAuthorName,
     required this.timestamp,
     required this.type,
   });
+
+  String get displayAuthorName => author?.name ?? legacyAuthorName ?? 'System';
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'text': text,
-      'authorName': authorName,
+      'author': author?.toJson(),
       'timestamp': timestamp.toIso8601String(),
       'type': type.name,
     };
   }
 
   factory ActivityLog.fromJson(Map<String, dynamic> json) {
+    final authorJson = json['author'];
+    final legacyAuthorName = json['authorName'] as String?;
+
     return ActivityLog(
       id: json['id'] as String,
       text: json['text'] as String,
-      authorName: json['authorName'] as String? ?? 'System',
+      author: authorJson is Map<String, dynamic>
+          ? User.fromJson(authorJson)
+          : null,
+      legacyAuthorName: legacyAuthorName == 'System' ? null : legacyAuthorName,
       timestamp: DateTime.parse(json['timestamp'] as String),
       type: ActivityType.values.byName(json['type'] as String? ?? 'comment'),
     );
